9e72ff43e8520b512b30b86ec6ec1b3aec16dfc4
Gabriel Gouvea <103601293+gouvea77@users.noreply.github.com>
2024-03-24 22:14:40 -0300

    scroll suave adição
    scroll suave adição

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+function efeitoEscrever() {
+  function typeWriteR(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = "";
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 30 * i);
+    });
+  }
+
+  const titulo = document.querySelector(".texto")
+  typeWriteR(titulo);
+}
+efeitoEscrever();
 
 
-function typeWriteR(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML ="";
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 30*i);
+function scrollSuave (){
+  const linksInternos = document.querySelectorAll(".js-menu");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection)
   });
 }
-
-const titulo = document.querySelector(".texto")
-typeWriteR(titulo);
-
+scrollSuave();
