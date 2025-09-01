53fd06fc298639eb70ba528a08375a7c4febc726
gouvea77 <gabrielgouvea77@hotmail.com>
2025-09-01 02:14:09 -0300

    att

--- js/modules/fetch-github.js
+++ js/modules/fetch-github.js
@@ -1,8 +1,9 @@
 export default function fetchGitHub() {
   let fotoPerfil = document.querySelector(".foto-perfil")
 
-  fetch("https://api.github.com/users/gouvea77/repos")
+  fetch("https://api.github.com/users/gouvea77")
   .then(response => response.json())
-  .then(response => fotoPerfil.src = response.owner.avatar_url)
+  .then(response => fotoPerfil.src = response.avatar_url)
+
 }
 
